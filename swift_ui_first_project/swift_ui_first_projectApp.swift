ccf3411ec130f2effea58a0372ac6cb34f2ca780
Muhammet <muhammetvural@anadolu.edu.tr>
2023-05-24 09:22:21 +0300

    Initial Commit

---
+++ swift_ui_first_project/swift_ui_first_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_ui_first_projectApp.swift
+//  swift_ui_first_project
+//
+//  Created by Muhammet Vural on 24.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct swift_ui_first_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
